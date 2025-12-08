6b215491498912e2c88e826045a5c4922f575a04
Bronzeyy <shen.andy@northeastern.edu>
2025-12-08 17:21:18 -0500

    Compress all mock data SQL files into one

--- database-files/CATEGORY_MOCK_DATA.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO category (categoryName, categoryID) VALUES ('Clothing', '1');
-INSERT INTO category (categoryName, categoryID) VALUES ('Electronics', '2');
-INSERT INTO category (categoryName, categoryID) VALUES ('Water bottles', '3');
-INSERT INTO category (categoryName, categoryID) VALUES ('ID Cards', '4');
